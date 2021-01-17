5b3091f545735c23586789d6ff0cac9f04e49731
Niclas Morin <brydling@gmail.com>
2021-01-17 17:37:04 +0100

    Added --import, --export, --destroy, --remove, --probe and possibility to specify a list of disks. Also fixed removal of old snapshots, restructured a bit and almost certainly fixed some bug that I've forgotten now.

--- common.py
+++ common.py
@@ -1,7 +1,26 @@
 import os.path
+import json
 
 import pool
 import luks
+
+settings_filepath = str()
+settings = None
+
+def get_settings():
+    return settings
+
+def read_settings(filepath):
+    global settings_filepath
+    global settings
+    
+    with open(filepath) as json_file:
+        settings = json.load(json_file)
+        settings_filepath = filepath
+
+def save_settings():
+    with open(settings_filepath,'w') as json_file:
+        json.dump(settings, json_file, indent=3)
 
 def open_luks_and_import_pool(disk, print_depth):
     name = disk["zpool"]
@@ -32,6 +51,8 @@ def export_pool_and_close_luks(disk, print_depth):
     if pool.pool_is_imported(name):
         print("  "*print_depth + "Exporting pool: " + name)
         pool.export_pool(name)
+    else:
+        print("  "*print_depth + "Pool already exported: " + name)
 
     luks_path = "/dev/mapper/" + luksname
     if os.path.exists(luks_path):
@@ -39,3 +60,5 @@ def export_pool_and_close_luks(disk, print_depth):
         retval,errormsg = luks.luksclose(luksname)
         if retval != 0:
             raise Exception(errormsg)
+    else:
+        print("  "*print_depth + "LUKS container already closed: " + luks_path)
